5d0268aa4d04eff368fbd1837080a3ee846e99d7
drazicc <drazicc@wwu.edu>
2020-03-04 18:42:57 -0800

    fixed...?

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -49,6 +49,49 @@ export default class Home extends Component {
               <TabPanel>
                 <h2 class='tabBody'>Info for Prospective Students</h2>
                 <p>
+                  General Application Questions:
+                  <ul>
+                    <li>Am I eligible to apply?</li>
+                    <li>Do I need a degree in Computer Science to be admitted to the program?</li>
+                    <li>Can I apply to start in Winter or Spring quarter?</li>
+                    <li>How do I apply? (What documents do I need to submit for my application?)</li>
+                    <li>What are the deadlines for the application?</li>
+                    <li>(What criteria are used to evaluate applicants for admission to the department?)</li>
+                  </ul>
+
+                  Language Proficiency Tests:
+                  <ul>
+                    <li>Do I need to take the GRE test?</li>
+                    <li>Are there minimum GRE scores required for admission and can I submit my GRE scores after the deadline?</li>
+                    <li>Do I need to take the TOEFL test? Are there minimum TOEFL/IELTS scores and can I submit my scores after the deadline?</li>
+                    <li>When should I take the GRE/TOEFL?</li>
+                    <li>I am currently in the CS honors program and thinking to apply for the Graduate Program. How should I apply?</li>
+                  </ul>
+                  Transfer Questions:
+                  <ul>
+                    <li>If I already have a MS in CS from another institute, may I apply to the CS MS program at WWU?</li>
+                    <li>If I am currently enrolled in a graduate program at another institution. May I apply for the CS MS program at WWU? Are my credits transferable?</li>
+                  </ul>
+                  If admitted:
+                  <ul>
+                    <li>If I am admitted to the program, can I defer admission?</li>
+                    <li>Can a course I took somewhere else (e.g., an online program) be waived from the prerequisite course list I will be required to take?</li>
+                    <li>What are the MS degree requirements?</li>
+                    <li>How long does it take to complete the program? Can I complete my degree part time?</li>
+                    <li>How can I find out more about course requirements for the CS Master’s program?</li>
+                  </ul>
+                  Fees and Funding/Financial Support:
+                  <ul>
+                    <li>What are tuition and fees? What if I’m an international student?</li>
+                    <li>Do you offer financial support for MS students?</li>
+                  </ul>
+                  Recommendation Letters:
+                  <ul>
+                    <li>How should my recommenders send their letters?</li>
+                    <li>I’ve been working for several years and it’s difficult to get 3 letters of recommendation from former professors. Will you accept letters from my employer or supervisors?</li>
+                  </ul>
+                </p>
+                <p>
                   Admissions and program details can be found here:
                   <a href='https://gradschool.wwu.edu/computer-science'>
                     {' '}
@@ -80,6 +123,27 @@ export default class Home extends Component {
               </TabPanel>
               <TabPanel>
                 <h2 class='tabBody'>Info for Current Students</h2>
+                <p>
+                  Academic Resources
+                  <ul>
+                    <li>I am starting my first quarter. What should I do?</li>
+                    <li>How many courses a quarter should I take?</li>
+                    <li>What are the basic requirements for the degree?</li>
+                    <li>Can the credits I took in BS count toward my MS?</li>
+                    <li>As a graduate student, can I take a 400 level course and use it to fulfill my MS degree requirement?</li>
+                    <li>What should I do if I want to register for the research course (CSCI 692)?</li>
+                    <li>I am planning to graduate next quarter. What is the graduation application and approval process?</li>
+                    <li>What should I do if I need to change my plan of study?</li>
+                    <li>What should I do if I want to take an independent study?</li>
+                    <li>Forms</li>
+                  </ul>
+                  TAships and other funding opportunities:
+                  <ul>
+                    <li>Graduate School Teaching Assistant Guidelines</li>
+                    <li>What awards are available from the graduate school?</li>
+                    <li>What awards are available from the CS department?</li>
+                  </ul>
+                </p>
                 <p>
                   Important to-dos for your first and last quarter: (not done)
                 </p>
